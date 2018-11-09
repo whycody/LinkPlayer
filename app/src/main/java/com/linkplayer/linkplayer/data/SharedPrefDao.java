9b1a1e09824d92e156b07ec8b3c95b847aadc168
Oktawian Kausz <play26400@gmail.com>
2018-11-09 20:50:51 +0100

    Added SharedPrefDao class

--- app/src/main/java/com/linkplayer/linkplayer/data/SharedPrefDao.java
+++ app/src/main/java/com/linkplayer/linkplayer/data/SharedPrefDao.java
@@ -1,3 +1,24 @@
+package com.linkplayer.linkplayer.data;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefDao {
+
+    private SharedPreferences sharedPreferences;
+    private final String PREFERENCES = "preferences";
+
+    public SharedPrefDao(Context context){
+        sharedPreferences =  context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    public void saveBooleanValue(String name, boolean value){
+        SharedPreferences.Editor preferencesEditor = sharedPreferences.edit();
+        preferencesEditor.putBoolean(name, value);
+        preferencesEditor.apply();
+    }
+
     public boolean getBooleanValue(String value){
         return sharedPreferences.getBoolean(value, false);
     }
+}
